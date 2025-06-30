edfb77c9c42eca51f8c314b7e43578eccaa6fef8
Imani Henderson <hendersoni@meta.com>
2025-06-30 16:51:13 -0700

    Navbar feature implemented with sidebar planning for next pr

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+    // usestate for sidebar will go here
+
+    return (
+        <nav className="flex items-center justify-between bg-white shadow px-4 py-3">
+            {/* will be using this button to trigger sidebar*/}
+            <button> 
+                <FaBars className="text-xl text-gray-700" />
+            </button>
+
+            <div className="flex-grow mx-4">
+                <input 
+                type="text"
+                placeholder="Search..."
+                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+            </div>
+        </nav>
+
+        // will be checking for sidebar conditions here 
+
+    )
+}
