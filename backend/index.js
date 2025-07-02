fe39ec91a57a831ce79378d707aa22ffc08ac1c9
Imani Henderson <hendersoni@meta.com>
2025-07-02 11:51:33 -0700

    plaid integration completed, and prisma updated internally, routes fixed, and authentication flow properly implemented.

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const cors = require('cors')
-const userRouter = require('./routes/user')
+const cors = require("cors");
+const userRouter = require("./routes/user");
+const plaidRouter = require("./routes/plaidRoutes");
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 
 // enables cors for all routes
-app.use(cors())
+app.use(cors());
 
 // user routes
-app.use('/user', userRouter)
+app.use("/user", userRouter);
+
+//plaid routes
+app.use("/plaid", plaidRouter);
 
 // main route
-app.get('/', (req, res) => {
-    res.send('Backend of Fina!')
+app.get("/", (req, res) => {
+  res.send("Backend of Fina!");
 });
 
 app.listen(PORT, () => {
   console.log(`Fina is running on port ${PORT}`);
 });
-
--- backend/routes/plaidRoutes.js
+++ backend/routes/plaidRoutes.js
@@ -1,23 +1,10 @@
-const firebaseAuth = require('./middleware/auth');
 const express = require('express');
-const plaidClient = require('./plaidClient');
-const app = express();
-app.use(express.json());
+const router = express.Router();
+const verifyFirebaseToken = require('../middleware/auth');
+const { createLinktoken, exchangePublicToken, getConnectionStatus } = require('../plaidControllers');
 
-app.post('', async (req, res) => {
-    try{
-        const response = await plaidClient.linkTokenCreate({
-            user: {
-                client_user_id: '',
-            },
-            client_name: 'My App',
-            products: ['auth', 'transactions'],
-            country_codes: ['US'],
-            language: 'en',
-        });
-        res.json({ link_token: response.data.link_token });
-    }   catch(error) {
-        console.error('Error creating link token:', error.response?.data || error);
-        res.status(500).json({ error: 'Failed to create link token' });
-    }
-});
+router.post('/create-link-token', verifyFirebaseToken, createLinktoken);
+router.post('/exchange-public-token', verifyFirebaseToken, exchangePublicToken);
+router.get('/connection-status', verifyFirebaseToken, getConnectionStatus);
+
+module.exports = router;
