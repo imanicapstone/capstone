2a889195c4f780a16eb2cd2057bfeceaa2f3e738
Imani Henderson <hendersoni@meta.com>
2025-07-07 14:55:55 -0700

    changes to routes fro reminder implementation

--- backend/routes/plaidRoutes.js
+++ backend/routes/plaidRoutes.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verifyFirebaseToken = require('../middleware/auth');
-const { createLinktoken, exchangePublicToken, getConnectionStatus } = require('../plaidControllers');
+const verifyFirebaseToken = require("../middleware/auth");
+const {
+  createLinktoken,
+  exchangePublicToken,
+  getConnectionStatus,
+  getTransactions,
+} = require("../plaidControllers");
 
-router.post('/create-link-token', verifyFirebaseToken, createLinktoken);
-router.post('/exchange-public-token', verifyFirebaseToken, exchangePublicToken);
-router.get('/connection-status', verifyFirebaseToken, getConnectionStatus);
+router.post("/create-link-token", verifyFirebaseToken, createLinktoken);
+router.post("/exchange-public-token", verifyFirebaseToken, exchangePublicToken);
+router.get("/connection-status", verifyFirebaseToken, getConnectionStatus);
+
+router.get("/transactions", verifyFirebaseToken, getTransactions);
 
 module.exports = router;
